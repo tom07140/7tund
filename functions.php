e8ce832af8f4a794557319916e1278747572c60b
Toomas Lõõnik <toomloo@greeny.cs.tlu.ee>
2015-10-14 10:26:43 +0300

    lisasin eelmise tunni näite

---
+++ functions.php
@@ -0,0 +1,84 @@
+<?php
+	// functions.php
+	// siia tulevd funktsioonid, kõik mis seotud AB'ga
+	
+	// Loon AB'i ühenduse
+	require_once("../configGlobal.php");
+	$database = "if15_toomloo_3";
+	
+	function getCarData(){
+		
+		$mysqli = new mysqli($GLOBALS["servername"], $GLOBALS["server_username"], $GLOBALS["server_password"], $GLOBALS["database"]);
+		
+		$stmt = $mysqli->prepare("SELECT id, user_id, number_plate, color FROM car_plates WHERE deleted IS NULL");
+		$stmt->bind_result($id, $user_id_from_database, $number_plate, $color);
+		$stmt->execute();
+		
+		// tekitan tühja massiivi, kus edaspidi hoian objekte
+		$car_array= array();
+		
+		// tee midagi seni, kuni saame ab'st ühe rea andmeid
+		while($stmt->fetch()){
+			// seda siin sees tehakse nii mitu korda kuni on ridu
+			
+			//tekitan objekti, kus hakkan hoidma väärtusi
+			$car = new StdClass();
+			$car->id = $id;
+			$car->plate = $number_plate;
+			$car->color = $color;
+			$car->user_id = $user_id_from_database;
+			
+			// lisan massiivi ühe rea juurde
+			array_push($car_array, $car);
+			// var_dump ütleb muutuja nime ja stuffi
+			//echo "<pre>";
+			//var_dump($car_array);
+			//echo "</pre><br>";
+		}
+		
+		// tagastan massiivi, kus kõik read sees
+		return $car_array;
+		
+		$stmt->close();
+		$mysqli->close();
+	}
+	
+	function deleteCar($id){
+		
+		$mysqli = new mysqli($GLOBALS["servername"], $GLOBALS["server_username"], $GLOBALS["server_password"], $GLOBALS["database"]);
+		
+		$stmt = $mysqli->prepare("UPDATE car_plates SET deleted=NOW() WHERE id=?");
+		$stmt->bind_param("i", $id);
+		if($stmt->execute()){
+			// sai kustutatud
+			// kustutame aadressirea tühjaks
+			header("Location: table.php");
+			
+		}
+		
+		$stmt->close();
+		$mysqli->close();
+	}
+	
+	function updateCar($id, $number_plate, $color){
+		
+		$mysqli = new mysqli($GLOBALS["servername"], $GLOBALS["server_username"], $GLOBALS["server_password"], $GLOBALS["database"]);
+		
+		$stmt = $mysqli->prepare("UPDATE car_plates SET number_plate=?, color=? WHERE id=?");
+		$stmt->bind_param("ssi", $number_plate, $color, $id);
+		if($stmt->execute()){
+			// sai uuendatud
+			// kustutame aadressirea tühjaks
+			//header("Location: table.php");
+			
+		}
+		
+		$stmt->close();
+		$mysqli->close();
+		
+	}
+	
+	
+	
+	
+?>
---
+++ table.php
@@ -0,0 +1,82 @@
+<?php
+	
+	require_once("functions.php");
+	
+	// kas kustutame
+	// ?delete=vastav id mida kustutame on aadressireal
+	if(isset($_GET["delete"])){
+		
+		echo "kustutame id ".$_GET["delete"];
+		//käivitan funktsiooni, saadan kaasa id
+		deleteCar($_GET["delete"]);
+		
+	}
+	
+	// salvestan muudatuse andmebaasi
+	if(isset($_POST["save"])){
+		
+		updateCar($_POST["id"], $_POST["number_plate"], $_POST["color"]);
+		
+	}
+
+	// käivitan funktsiooni
+	$array_of_cars = getCarData();
+	
+	// trükin välja esimese auto
+	//echo $array_of_cars[1]->id." ".$array_of_cars[1]->plate;
+
+?>
+
+<h2>Tabel</h2>
+<table border=1>
+	<tr>
+		<th>id</th>
+		<th>numbrimärk</th>
+		<th>värv</th>
+		<th>user_id</th>
+		<th>kustuta</th>
+		<th>edit</th>
+	</tr>
+	
+	<?php
+		// trükime välja read
+		// massiivi pikkus count()
+		for($i = 0;$i < count($array_of_cars);$i++){
+			//echo $array_of_cars[$i]->id;
+			
+			// kasutaja tahab muuta seda rida
+			if(isset($_GET["edit"]) && $array_of_cars[$i]->id == $_GET["edit"]){
+				
+				echo "<tr>";
+				echo "<form action='table.php' method='post'>";
+				echo "<input type='hidden' name='id' value='".$array_of_cars[$i]->id."'>";
+				echo "<td>".$array_of_cars[$i]->id."</td>";
+				echo "<td><input name='number_plate' value='".$array_of_cars[$i]->plate."'></td>";
+				echo "<td><input name='color' value='".$array_of_cars[$i]->color."'></td>";
+				echo "<td>".$array_of_cars[$i]->user_id."</td>";
+				echo "<td><a href='table.php'>cancel</a></td>";
+				echo "<td><input type='submit' name='save'></td>";
+				echo "</form>";
+				echo "</tr>";
+				
+				
+			}else{
+				echo "<tr>";
+				echo "<td>".$array_of_cars[$i]->id."</td>";
+				echo "<td>".$array_of_cars[$i]->plate."</td>";
+				echo "<td>".$array_of_cars[$i]->color."</td>";
+				echo "<td>".$array_of_cars[$i]->user_id."</td>";
+				echo "<td><a href='?delete=".$array_of_cars[$i]->id."'>X</a></td>";
+				echo "<td><a href='?edit=".$array_of_cars[$i]->id."'>edit</a></td>";
+				echo "</tr>";
+				
+				
+			}
+			
+		
+		}
+	
+	
+	?>
+
+</table>
